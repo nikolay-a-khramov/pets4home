66bc67d50f68a9c2956d403b40eb16c56ea2a5d7
nikolay-a-khramov <34689601+nikolay-a-khramov@users.noreply.github.com>
2018-07-13 16:50:36 +0300

    Changes in the main class to test other classes

--- AdvertRefresh.cs
+++ AdvertRefresh.cs
@@ -4,59 +4,51 @@ using System.Collections.Generic;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using pets4home.core;
+using log4net.Config;
+using log4net;
+
 namespace pets4home
 {
     class AdvertRefresh
     {
-        private static readonly String[] ADS_TO_REFRESH_TITLES = {"Fish for Sale"};
+        private static readonly String AD_TO_REFRESH_TITLE = "Fish for Sale";
         private static readonly String LOGIN = "christian_yeates@yahoo.co.uk";
         private static readonly String PASS = "Petspassword1";
-        private static readonly String URL_BASE = "https://www.pets4homes.co.uk/login/";
-        private static readonly String URL_MANAGE_ADS = "https://www.pets4homes.co.uk/account/manage-adverts/";
-        private static readonly By LOCATOR_FLD_LOGIN = By.Id("email");
-        private static readonly By LOCATOR_FLD_PASS = By.Id("pass");
-        private static readonly By LOCATOR_BTN_LOGIN = By.CssSelector(".btn-lg");
-        private static readonly By LOCATOR_DIV_AD = By.CssSelector(".manageadvert");
-        private static readonly By LOCATOR_DIV_AD_TITLE = By.CssSelector(".title");
-        private static readonly By LOCATOR_ICN_REFRESH = By.CssSelector(".fa-refresh");
+
+        private static readonly Scheduler scheduler = new Scheduler();
+
         static void Main(string[] args)
         {
-            ChromeDriverService serviceCR = ChromeDriverService.CreateDefaultService();
-            serviceCR.Port = 9510;
-            IWebDriver driver = new ChromeDriver(serviceCR);
-            driver.Url = URL_BASE;
+            var logRepository = LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new System.IO.FileInfo("log4net.config"));
 
-            IWebElement fieldLogin = driver.FindElement(LOCATOR_FLD_LOGIN);
-            IWebElement fieldPass = driver.FindElement(LOCATOR_FLD_PASS);
-            IWebElement btnLogin = driver.FindElement(LOCATOR_BTN_LOGIN);
+            //ChromeDriverService serviceCR = ChromeDriverService.CreateDefaultService();
+            //serviceCR.Port = 9510;
+            //IWebDriver driver = new ChromeDriver(serviceCR);
 
-            fieldLogin.SendKeys(LOGIN);
-            fieldPass.SendKeys(PASS);
-            btnLogin.Click();
+            Advert fishAdCarl = new Advert(LOGIN, PASS, AD_TO_REFRESH_TITLE, TimeSpan.FromSeconds(20));
+            fishAdCarl.FirstName = "Carl";
+            fishAdCarl.LastName = "Smith";
+            fishAdCarl.Enabled = true;
+            scheduler.AddTask(fishAdCarl);
 
-            driver.Url = URL_MANAGE_ADS;
-            //TODO: Revert change!
-            List<IWebElement> divAds = findAdWithTitle(driver, new List<String>(ADS_TO_REFRESH_TITLES));
+            Advert fishAdMike = new Advert(LOGIN, PASS, AD_TO_REFRESH_TITLE, TimeSpan.FromSeconds(21));
+            fishAdMike.FirstName = "Mike";
+            fishAdMike.LastName = "Johnson";
+            fishAdMike.Enabled = true;
+            scheduler.AddTask(fishAdMike);
 
-            foreach(IWebElement ad in divAds) {
-                IWebElement iconRefresh = ad.FindElement(LOCATOR_ICN_REFRESH);
-                (new Actions(driver)).MoveToElement(iconRefresh).Click().Perform();
-            }
-            
-            driver.Quit();
+            Console.WriteLine("Press enter to stop Mike's task");
+            Console.ReadLine();
+
+            scheduler.RemoveTask(fishAdMike);
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+
+            scheduler.RemoveAllTasks();
         }
 
-        static List<IWebElement> findAdWithTitle(IWebDriver driver, List<String> titlesToFind) {
-            List<IWebElement> foundDivs = new List<IWebElement>();
-            var allAdDivs = driver.FindElements(LOCATOR_DIV_AD);
-            foreach (IWebElement div in allAdDivs) {
-                IWebElement divTitle = div.FindElement(LOCATOR_DIV_AD_TITLE);
-                Console.WriteLine(divTitle.Text);
-                if (titlesToFind.Contains(divTitle.Text)) {
-                    foundDivs.Add(div);
-                }
-            }
-            return foundDivs;
-        }
     }
 }
